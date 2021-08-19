4feeeb330d5714bf178bd127b3147a0f3427cf4f
Ankit <ankitv397@gmail.com>
2021-08-19 16:09:32 +0530

    Exercise 2 completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,21 @@
+import java.util.*
+
 /**
  * Main function where the execution starts
  *
  */
 fun main() {
-    val year = 2000
-    if (isLeapYear(year)) println("$year is a leap year") else println("$year is not a leap year")
+    val string = "nsmlpoiquwrayzcbmnxkalsgetabdjhwgru ^iweyghfj& bdnjvbn mzx"
+    println("String: $string is a pangram?  ${isPangram(string)} ")
 }
 
 /**
- * Returns a Boolean whether a year is leap year
+ * Returns a Boolean whether a string is pangram
  *
- * @param year The year which is to be analysed
- * @return Boolean Whether given year is a leap year or not.
+ * @param string The string which is to be analysed
+ * @return Boolean Whether given string is a pangram or not.
  */
-fun isLeapYear(year: Int): Boolean {
-    if (year % 400 == 0)
-        return true
-    if (year % 100 == 0)
-        return false
-    if (year % 4 == 0)
-        return true
-    return false
+fun isPangram(string: String): Boolean{
+    val lowerString = string.lowercase(Locale.getDefault())
+    return ('a'..'z').all { it in lowerString }
 }
